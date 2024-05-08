7482ea74a0d729d434c6496fd09a63b2a645b75d
ABDELAZiZ EL ARASSi <hi@aelarassi.com>
2024-05-08 16:27:19 +0100

    Initial Commit

---
+++ Unit Testing Swift/Unit_Testing_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_Testing_SwiftApp.swift
+//  Unit Testing Swift
+//
+//  Created by ABDELAZiZ EL ARASSi on 8/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Unit_Testing_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
